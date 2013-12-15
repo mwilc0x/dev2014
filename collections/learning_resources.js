d2f9245064cbe439eef636becccc6f18d202fdf3
Mike Wilcox <mwilcox56@gmail.com>
2013-12-14 19:52:25 -0500

    removed insecure and add conditions to allow users to submit resources

--- collections/learning_resources.js
+++ collections/learning_resources.js
@@ -1,2 +1,8 @@
 // eliminate using var to make LearningResources available to whole app
 LearningResources = new Meteor.Collection('learningResources');
+LearningResources.allow({
+    insert: function(userId, doc) {
+        // only allow posting if you are logged in
+        return !! userId;
+    }
+});
