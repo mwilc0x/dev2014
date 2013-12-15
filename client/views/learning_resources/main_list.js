ba95ae0b21b01889e2709512a922c69a59238f87
Mike Wilcox <mwilcox56@gmail.com>
2013-12-14 19:42:47 -0500

    add missing files

---
+++ client/views/learning_resources/main_list.js
@@ -0,0 +1,8 @@
+// In Meteor, find() returns a cursor, which is a reactive data source.
+// When we want to log its contents, we can then use fetch() on that cursor to transform it into an array .
+
+Template.mainList.helpers({
+    items: function() {
+        return LearningResources.find();
+    }
+});
