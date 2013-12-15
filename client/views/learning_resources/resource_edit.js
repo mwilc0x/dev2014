249bafb90c4e61efe9fecf9f06c7e4d2c7c6e79b
Mike Wilcox <mwilcox56@gmail.com>
2013-12-14 20:53:54 -0500

    add template and manager to edit a learning resource

---
+++ client/views/learning_resources/resource_edit.js
@@ -0,0 +1,31 @@
+Template.resourceEdit.events({
+    'submit form': function(e) {
+        e.preventDefault();
+
+        var currentResourceId = this._id;
+
+        var resourceProperties = {
+            url: $(e.target).find('[name=url]').val(),
+            title: $(e.target).find('[name=title]').val()
+        }
+
+        LearningResources.update(currentResourceId, {$set: resourceProperties}, function(error) {
+            if (error) {
+                // display the error to the user
+                throwError(error.reason);
+            } else {
+                Router.go('resourcePage', {_id: currentResourceId});
+            }
+        });
+    },
+
+    'click .delete': function(e) {
+        e.preventDefault();
+
+        if (confirm("Delete this learning resource?")) {
+            var currentResourceId = this._id;
+            LearningResources.remove(currentResourceId);
+            Router.go('mainList');
+        }
+    }
+});
