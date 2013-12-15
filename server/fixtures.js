e77be45a789bd6f16b0699d572b2eb740fd59713
Mike Wilcox <mwilcox56@gmail.com>
2013-12-14 21:25:24 -0500

    add comments

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,27 +1,56 @@
 // code executes immediately when the server starts
 
+// Fixture data
 if (LearningResources.find().count() === 0) {
-    LearningResources.insert({
+    var now = new Date().getTime();
+
+    // create two users
+    var mikeId = Meteor.users.insert({
+        profile: { name: 'Mike Wilcox' }
+    });
+    var mike = Meteor.users.findOne(mikeId);
+    var sachaId = Meteor.users.insert({
+        profile: { name: 'Sacha Greif' }
+    });
+    var sacha = Meteor.users.findOne(sachaId);
+
+    var telescopeId = LearningResources.insert({
         title: 'Introducing Telescope',
-        author: 'Sacha Greif',
-        url: 'http://sachagreif.com/introducing-telescope/'
+        userId: sacha._id,
+        author: sacha.profile.name,
+        url: 'http://sachagreif.com/introducing-telescope/',
+        submitted: now - 7 * 3600 * 1000
+    });
+
+    Comments.insert({
+        postId: telescopeId,
+        userId: mike._id,
+        author: mike.profile.name,
+        submitted: now - 5 * 3600 * 1000,
+        body: 'This is a good resource.'
+    });
+
+    Comments.insert({
+        postId: telescopeId,
+        userId: sacha._id,
+        author: sacha.profile.name,
+        submitted: now - 3 * 3600 * 1000,
+        body: 'I agree!'
     });
 
     LearningResources.insert({
         title: 'Meteor',
-        author: 'Tom Coleman',
-        url: 'http://meteor.com'
+        userId: mike._id,
+        author: mike.profile.name,
+        url: 'http://meteor.com',
+        submitted: now - 10 * 3600 * 1000
     });
 
     LearningResources.insert({
         title: 'The Meteor Book',
-        author: 'Tom Coleman',
-        url: 'http://themeteorbook.com'
-    });
-
-    LearningResources.insert({
-        title: 'AngularJS',
-        author: 'Brad Green, Shyam Seshadri',
-        url: 'http://shop.oreilly.com/product/0636920028055.do'
+        userId: mike._id,
+        author: mike.profile.name,
+        url: 'http://themeteorbook.com',
+        submitted: now - 12 * 3600 * 1000
     });
 }
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,7 @@
 Meteor.publish('learningResources', function() {
     return LearningResources.find();
 });
+
+Meteor.publish('comments', function() {
+    return Comments.find();
+});
